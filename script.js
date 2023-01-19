c40750d498c4e1a268b8ae2725cba313191701c1
gabisaka <gabrielayts@hotmail.com>
2023-01-18 20:20:23 -0400

    Atualização JS e HTML

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup(form)
 
 const data = {
-  Academia: ["01-01", "01-02", "01-03"],
+  Academia: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-06"],
   Remedio: ["01-03"],
   Estudo: ["01-01", "01-02", "01-03"],
 }
