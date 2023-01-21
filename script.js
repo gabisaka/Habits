790c6786c9069fd267d762e133a9aedea8b644cc
gabisaka <gabrielayts@hotmail.com>
2023-01-20 20:38:11 -0400

    Salvar os dados

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const nlwSetup = new NLWSetup(form)
 const button = document.querySelector("header button")
 
 button.addEventListener("click", add)
+form.addEventListener("change", save)
 
 function add() {
-  const today = "01/01"
-
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
   const dayExists = nlwSetup.dayExists(today)
 
   if (dayExists) {
@@ -14,8 +14,14 @@ function add() {
     return
   }
 
-  nlwSetup.addDay("01/01")
+  alert("Adicionado com sucesso")
+  nlwSetup.addDay(today)
 }
 
-/* nlwSetup.setData(data)
-nlwSetup.load() */
+function save() {
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits"))
+nlwSetup.setData(data)
+nlwSetup.load()
